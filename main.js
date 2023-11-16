23a9e83b4e25e17d737763791753a9e923cf3f2f
Yann <fauquembergueyann@gmail.com>
2023-11-16 11:09:44 +0100

    Exo7

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ var factoriel = 0;
 
 for (var i = 1; i <= N; i++)
 {
-    factoriel += i;
+    factoriel = Addition(factoriel, i);
 }
 
 alert("Le calcul fait " + factoriel);
+
+function Addition (a, b)
+{
+    return a + b;
+}
