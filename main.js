ac6735402771f1315ef61e487bd62f998496a75e
Yann <fauquembergueyann@gmail.com>
2023-11-16 10:32:22 +0100

    Exo4

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-var MavariableString = "toto";
-var MavariableNumerique = 100;
-var Mavaraibletableau = ["Apple", "Banana"];
-var MavariableObjet = window;
+// alert("toto");
 
-alert(MavariableString + MavariableNumerique + Mavaraibletableau[0] + MavariableObjet.location.pathname);
+var MavariableString = "toto";
+// var MavariableNumerique = 100;
+// var Mavaraibletableau = ["Apple", "Banana"];
+// var MavariableObjet = window;
+// alert(MavariableString + MavariableNumerique + Mavaraibletableau[0] + MavariableObjet.location.pathname);
+
+var MavariableNumerique = prompt("entrez un nombre", 100);
+
+if (MavariableString == "toto" && MavariableNumerique != 200 || MavariableNumerique <= 100)
+{
+    alert("je suis le cas 1");
+}
+else {
+    alert("je suis le cas 2");
+}
