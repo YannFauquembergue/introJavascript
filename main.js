54771ca2bc91bac1bae6290505336babd0589d32
Yann <fauquembergueyann@gmail.com>
2023-11-29 08:51:56 +0100

    Exo10 fin

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 var MaDiv = document.getElementById("MaDiveNum1");
     
 MaDiv.addEventListener("click", UneProcedureQuiChangeLetext);
+MaDiv.addEventListener("mouseover", UneProcedureQuiSurvole);
+MaDiv.addEventListener("mouseout", UneProcedureQuiSurvolePlus);
 
 function UneProcedureQuiChangeLetext(evenement)
 {
     evenement.target.innerHTML = "On m'a cliqué dessus";
+    evenement.target.setAttribute("class", "MaDivModifiee");
+}
+
+function UneProcedureQuiSurvole(evenement)
+{
+    evenement.target.style.display = "block";
+}
+function UneProcedureQuiSurvolePlus(evenement)
+{
+    evenement.target.style.display = "none";
 }
